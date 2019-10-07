a8f5b9b4051ea365f8242d1dbc861c34d5b3440c
pjestioko <pauljoseph.estioko@gmail.com>
2019-10-06 21:00:25 -1000

    pop front

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-// PUSH FRONT
+// // PUSH FRONT
+
+// let arr = [1, 2, 3, 4, 5];
+// let val = 999;
+// pushFront(arr, val);
+// function pushFront(arr, val) {
+//     for (var i = 0; i > arr.length; i--) {
+//         arr[i + 1] = arr[i];
+//     }
+//     arr[0] = val;
+// }
+// console.log(arr);
+
+// POP FRONT
 
 let arr = [1, 2, 3, 4, 5];
-let val = 999;
-pushFront(arr, val);
-function pushFront(arr, val) {
-    for (var i = 0; i > arr.length; i--) {
-        arr[i + 1] = arr[i];
+function popFront(arr) {
+    var val = arr[0];
+    for (var index = 0; index < arr.length - 1; index++) {
+        var currentValue = arr[index + 1];
+        arr[index] = currentValue;
     }
-    arr[0] = val;
+    arr.length -= 1;
+    console.log(arr);
 }
-console.log(arr);
+console.log(popFront(arr));
