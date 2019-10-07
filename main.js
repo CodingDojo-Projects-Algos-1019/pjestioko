1e3c74e835ebb5ad5f45b77ca0a7535ffb0ce3bf
pjestioko <pauljoseph.estioko@gmail.com>
2019-10-06 20:25:10 -1000

    push forward

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-// TEST TEST TEST
+// PUSH FRONT
+
+let arr = [1, 2, 3, 4, 5];
+let val = 999;
+pushFront(arr, val);
+function pushFront(arr, val) {
+    for (var i = 0; i > arr.length; i--) {
+        arr[i + 1] = arr[i];
+    }
+    arr[0] = val;
+}
+console.log(arr);
